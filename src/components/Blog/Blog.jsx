dbe066547f2942659a2b2d2855a77f1811c8c75d
munnabiswas99 <biswas15-5261@diu.edu.bd>
2025-07-31 18:02:24 +0600

    blogs added

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,8 +1,31 @@
 const Blog = ({blog}) => {
+
+    const {title,cover, author, author_img, posted_date, reading_time, hashtags} = blog;
     return (
-        <div>
-            <h2>{blog.title}</h2>
-            
+        <div className="p-4 mb-4">
+            <img src={cover} alt={title} />
+            {console.log(cover)}
+            <div className="flex justify-between my-2">
+                <div className="flex items-center gap-2">
+                    <img className="w-10 h-10 rounded-full" src={author_img} alt={author} />
+                    <div>
+                        <h3>{author}</h3>
+                        <p>{posted_date}</p>
+                    </div>
+                </div>
+                <div className="flex items-center gap-2">
+                    <p>{reading_time} min read</p>
+                    <button className="bg-blue-500 text-white px-2 py-1 rounded">Add</button>
+                </div>
+            </div>
+            <h2>{title}</h2>
+            <p>
+                {
+                    hashtags.map((tag, index) => (
+                        <span key={index} className="bg-gray-200 px-2 py-1 rounded mr-2"><a href="">#{tag}</a></span>
+                    ))
+                }
+            </p>
         </div>
     );
 };
